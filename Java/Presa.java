700948677b3c7877f18966a423c24f7127e64218
Carlos Cosme <carlosjcosmem@gmail.com>
2019-02-15 16:22:10 +0000

    Java testing

---
+++ Java/Presa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caceria;
+
+/**
+ *
+ * @author pc
+ */
+public interface Presa {
+    public void Comer();
+    public void Beber();
+    public void Observar();
+    public void Huir();
+    
+}
